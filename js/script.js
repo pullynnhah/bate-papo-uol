89afe1f28959c305feff23d06323d4bb328a1352
Paula Rodrigues <pullynnhah@icloud.com>
2023-03-17 17:12:47 -0300

    :sparkles: implement login w/ enter

--- js/script.js
+++ js/script.js
@@ -6,6 +6,14 @@ function renderLogin() {
     <button onclick="login()">Entrar</button>
   </div>
   `;
+
+  const btn = document.querySelector("button");
+  document.querySelector("input").addEventListener("keypress", e => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      btn.click();
+    }
+  });
 }
 
 function login() {
@@ -36,20 +44,20 @@ function renderApp() {
   <header class="header-footer">
       <img src="./assets/logo.png" alt="Logo do Bate-Papo UOL" />
       <ion-icon name="people"></ion-icon>
-    </header>
-    <main>
-      <ul></ul>
-    </main>
-     <aside></aside>
-    <footer class="header-footer">
-      <div>
-        <input type="text" placeholder="Escreva aqui..." />
-        <p>
-          Enviando para <span class="receiver"></span> <span class="mode"></span>
-        </p>
-      </div>
-      <ion-icon name="paper-plane-outline"></ion-icon>
-    </footer>
+  </header>
+  <main>
+    <ul></ul>
+  </main>
+    <aside></aside>
+  <footer class="header-footer">
+    <div>
+      <input type="text" placeholder="Escreva aqui..." />
+      <p>
+        Enviando para <span class="receiver"></span> <span class="mode"></span>
+      </p>
+    </div>
+    <ion-icon name="paper-plane-outline"></ion-icon>
+  </footer>
 `;
 }
 
@@ -58,14 +66,27 @@ function getMessages() {
     .get(`${API_URL}/messages`)
     .catch(err => window.location.reload())
     .then(res => {
-      renderMessages(
-        res.data.filter(
-          msg =>
-            msg.type !== "private_message" || msg.from === message.from || msg.to === message.from
-        )
-      );
+      const messages = res.data.filter(isMessageVisible);
+      lastMessage = messages.at(-1);
+      if (!compareMessages(lastMessage, lastRenderMessage)) renderMessages(messages);
       setTimeout(getMessages, 3000);
     });
+}
+
+function compareMessages(msg1, msg2) {
+  const keys = Object.keys(msg1);
+  if (keys.length !== Object.keys(msg2).length) return false;
+  for (const key of keys) {
+    if (msg1[key] !== msg2[key]) return false;
+  }
+
+  return true;
+}
+
+function isMessageVisible(msg) {
+  if (msg.type !== "private_message") return true;
+  if (msg.from === message.from) return true;
+  return msg.to === message.from;
 }
 
 function renderMessages(messages) {
@@ -94,7 +115,11 @@ function renderMessages(messages) {
       `;
     }
   });
+
+  lastRenderMessage = messages.at(-1);
+  document.querySelector("main li:last-child").scrollIntoView();
 }
+
 const API_URL = "https://mock-api.driven.com.br/api/v6/uol";
 const root = document.querySelector("#root");
 
@@ -107,3 +132,6 @@ const message = {
   text: null,
   type: "message"
 };
+
+let lastRenderMessage = {};
+let lastMessage;
