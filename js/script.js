49a0397a2da399c052cd0e72b465d60aed8fee5e
Paula Rodrigues <pullynnhah@icloud.com>
2023-03-17 16:32:34 -0300

    :sparkles: implement render messages functionality

--- js/script.js
+++ js/script.js
@@ -19,6 +19,7 @@ function login() {
     .then(res => {
       message.from = name;
       renderApp();
+      getMessages();
       setTimeout(keepConnection, 5000);
     });
 }
@@ -31,9 +32,69 @@ function keepConnection() {
 }
 
 function renderApp() {
-  console.log("renderApp");
+  root.innerHTML = /*html*/ `
+  <header class="header-footer">
+      <img src="./assets/logo.png" alt="Logo do Bate-Papo UOL" />
+      <ion-icon name="people"></ion-icon>
+    </header>
+    <main>
+      <ul></ul>
+    </main>
+     <aside></aside>
+    <footer class="header-footer">
+      <div>
+        <input type="text" placeholder="Escreva aqui..." />
+        <p>
+          Enviando para <span class="receiver"></span> <span class="mode"></span>
+        </p>
+      </div>
+      <ion-icon name="paper-plane-outline"></ion-icon>
+    </footer>
+`;
 }
 
+function getMessages() {
+  axios
+    .get(`${API_URL}/messages`)
+    .catch(err => window.location.reload())
+    .then(res => {
+      renderMessages(
+        res.data.filter(
+          msg =>
+            msg.type !== "private_message" || msg.from === message.from || msg.to === message.from
+        )
+      );
+      setTimeout(getMessages, 3000);
+    });
+}
+
+function renderMessages(messages) {
+  const ul = document.querySelector("main ul");
+  ul.innerHTML = "";
+  messages.forEach(msg => {
+    if (msg.type === "status") {
+      ul.innerHTML += /*html*/ `
+      <li class="status-message">
+        <span class="time">(${msg.time})</span> <span class="from">${msg.from}</span> ${msg.text}
+      </li>
+      `;
+    } else if (msg.type === "message") {
+      ul.innerHTML += /*html*/ `
+      <li class="message">
+        <span class="time">(${msg.time})</span> <span class="from">${msg.from}</span> para
+        <span class="to">${msg.to}</span>: ${msg.text}
+      </li>
+      `;
+    } else {
+      ul.innerHTML += /*html*/ `
+      <li class="private-message">
+        <span class="time">(${msg.time})</span> <span class="from">msg.from</span> reservadamente para
+        <span class="to">msg.to</span>: ${msg.text}
+      </li>
+      `;
+    }
+  });
+}
 const API_URL = "https://mock-api.driven.com.br/api/v6/uol";
 const root = document.querySelector("#root");
 
